d17cdc5084f1e925424fb156794c105f53cd5d05
opatry <olivier.patry@gmail.com>
2013-10-09 23:07:53 +0200

    xUnit reports added to Jenkins

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "Hello World!"
 
-mkdir .build
+mkdir .build || exit $?
+mkdir .build/reports || exit $?
 pushd .build
 
 # debug to get code coverage.
 # TODO OP how have both coverage and release? two sequential builds?
 cmake -DCMAKE_BUILD_TYPE=Debug .. || exit $?
 make || exit $?
-./test/MyLib_GTest || exit $?
+./test/MyLib_GTest --gtest_output=xml:reports/xunit-report.xml || exit $?
 
 popd
 
