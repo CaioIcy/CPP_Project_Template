db0508464bfe376081ad079785b951a1ae7f1cf9
opatry <olivier.patry@gmail.com>
2013-10-09 23:04:51 +0200

    Try Jenkins CMake build

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 echo "Hello World!"
+
+mkdir .build
+pushd .build
+
+# debug to get code coverage.
+# TODO OP how have both coverage and release? two sequential builds?
+cmake -DCMAKE_BUILD_TYPE=Debug .. || exit $?
+make || exit $?
+./test/MyLib_GTest || exit $?
+
+popd
+
 exit $?
