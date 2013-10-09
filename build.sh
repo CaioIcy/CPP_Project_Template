796d01058dc25df2903bd716fa7130ff7b222068
opatry <olivier.patry@gmail.com>
2013-10-10 01:12:56 +0200

    Enable CPP Check (quick & dirty to access src/include path

--- build.sh
+++ build.sh
@@ -14,14 +14,18 @@ mkdir reports || exit $?
 # TODO OP how have both coverage and release? two sequential builds?
 cmake -DCMAKE_BUILD_TYPE=Debug .. || exit $?
 make || exit $?
-make doc || exit $?
 ./test/MyLib_GTest --gtest_output=xml:reports/xunit-report.xml || exit $?
 
-# TODO OP configure where .gcna files are stored
+# Code Coverage report (Cobertura) (handled by jenkins)
+# TODO OP configure where .gcna files are stored from CMake
 gcovr -x test/CMakeFiles/MyLib_GTest.dir/mylib > reports/gcov-report.xml || exit $?
 
-# TODO OP do something with the package (upload in an artifact repository?)
-make package || exit $?
+# CPP Check report (handled by jenkins)
+# TODO OP configure where includes and source are stored from CMake
+cppcheck -v --enable=all --xml ${BASE_DIR}/include ${BASE_DIR}/src 2> reports/cppcheck-report.xml || exit $?
+
+make doc || exit $? # included in archive
+make package || exit $? # archived by jenkins
 
 popd
 
