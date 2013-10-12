9b49c9db76e6b7c97b3d9f87a2a6c57c5aa41c76
opatry <olivier.patry@gmail.com>
2013-10-12 14:15:52 +0200

    Fix parent build dir behavior

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname "$(readlink -f $0)")
+BASE_NAME=$(basename "${BASE_DIR}")
 
-BUILD_DIR=${BASE_DIR}/../bin
+BUILD_DIR=${BASE_DIR}/../${BASE_NAME}_bin
 REPORTS_DIR=${BUILD_DIR}/reports
 
-rm -Rf ${BUILD_DIR} || exit $?
 mkdir -p ${BUILD_DIR} || exit $?
 pushd ${BUILD_DIR}
 
@@ -14,7 +14,7 @@ mkdir -p ${REPORTS_DIR} || exit $?
 # debug to get code coverage.
 # TODO OP refer to test binary output from CMake infos
 # TODO OP how have both coverage and release? two sequential builds?
-cmake -DCMAKE_BUILD_TYPE=Debug .. || exit $?
+cmake -DCMAKE_BUILD_TYPE=Debug ${BASE_DIR} || exit $?
 make || exit $?
 valgrind --xml=yes --xml-file=${REPORTS_DIR}/valgrind-report.xml test/MyLib_GTest --gtest_output=xml:${REPORTS_DIR}/xunit-report.xml || exit $?
 
