6da2a8e8b0c3ce6396b3ff517c92101d4c4506ce
CaioIcy <caio.nardelli@hotmail.com>
2014-12-17 23:52:44 -0200

    Checking for c++11 support in project main

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char** argv) {
 	std::cout << "*------Project template main!-------*" << std::endl;
 	std::cout << "*************************************" << std::endl;
 
+	// Making sure other classes are being linked properly
 	some_module::Feature feature;
 	const std::string testString = feature.toString();
 	std::cout << testString << std::endl;
@@ -17,6 +18,10 @@ int main(int argc, char** argv) {
 	Widget widget;
 	const int testMeaning = widget.life();
 	std::cout << testMeaning << std::endl;
+
+	// Testing C++11
+	void* ptr = nullptr;
+	((void)ptr);
 
 	return 0;
 }
