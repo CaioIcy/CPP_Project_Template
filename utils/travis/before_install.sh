556192aee4fc8d48083bc97f085c6f8a8712e2cb
CaioIcy <caio.nardelli@hotmail.com>
2015-01-28 02:00:23 -0200

    #2 :eight: Updating to doxygen 1.8

--- utils/travis/before_install.sh
+++ utils/travis/before_install.sh
@@ -3,8 +3,9 @@ set -o posix
 
 ##############################################################################
 # These are needed for the C++ Project Template
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test # g++-4.8
-sudo add-apt-repository --yes ppa:kubuntu-ppa/backports   # cmake over 2.8.7
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test	  # g++-4.8
+sudo add-apt-repository --yes ppa:kubuntu-ppa/backports		  # cmake over 2.8.7
+sudo apt-add-repository --yes ppa:libreoffice/libreoffice-4-2 # doxygen 1.8
 ##############################################################################
 
 ##############################################################################
